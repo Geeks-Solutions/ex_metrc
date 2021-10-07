e8c25c5f1980ac8b03fd55e3cb025cc91523f3d4
DaveElGhoul <91114952+DaveElGhoul@users.noreply.github.com>
2021-10-07 14:42:19 +0300

    restructure structs and removed deprecated fields

---
+++ lib/structs/employee_license_struct.ex
@@ -0,0 +1,6 @@
+defmodule EmployeeLicense do
+  defstruct number: "",
+            start_date: nil,
+            end_date: nil,
+            license_type: ""
+end
--- lib/structs/employee_struct.ex
+++ lib/structs/employee_struct.ex
@@ -1,4 +1,4 @@
 defmodule Employee do
   defstruct fullname: "",
-            license: %{}
+            license: %EmployeeLicense{}
 end
